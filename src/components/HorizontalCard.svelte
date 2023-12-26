e1fe00a9731722c4717efa7389343193a58373d5
Rayhan Egar <rresnn@gmail.com>
2023-12-26 20:33:23 +0700

    added images asset

---
+++ src/components/HorizontalCard.svelte
@@ -0,0 +1,17 @@
+<script>
+    // export const src = '../images/sample-1.jpg';
+    export const heading = 'Projects I Worked On';
+    export const paragraph = 'Here goes the description of the project';
+</script>
+
+<div class="container mx-auto px-4">
+    <div class="flex items-center justify-between">
+        <div class="w-1/2">
+            <!-- <img src={src} alt="Project Thumbnail"> -->
+        </div>
+        <div class="w-1/2">
+            <h3>{heading}</h3>
+            <p>{paragraph}</p>
+        </div>
+    </div>
+</div>
---
+++ src/components/Works.svelte
@@ -0,0 +1,11 @@
+<script>
+    import HorizontalCard from "./HorizontalCard.svelte";
+    import VerticalCard from "./VerticalCard.svelte";
+</script>
+
+<div class="container mx-auto px-10">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <HorizontalCard />
+        
+    </div>
+</div>
