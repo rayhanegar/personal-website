2bf709a502d1a370aa547dda05d5eb429e21bd2e
Rayhan Egar <rresnn@gmail.com>
2024-01-11 01:58:35 +0700

    Added vercel analytics, speed insight not workin yet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
   import "../app.css";
   import Navbar from "../components/Navbar.svelte";
+  import { dev } from '$app/environment';
+
+  import { inject } from '@vercel/analytics';
+  inject({ mode: dev ? 'development' : 'production' });
+
+  import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+  injectSpeedInsights();
 </script>
 
 <Navbar />
